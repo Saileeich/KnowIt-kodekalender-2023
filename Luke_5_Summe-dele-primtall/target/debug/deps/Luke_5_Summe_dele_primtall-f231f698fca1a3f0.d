e8f13521b366e9010045eb528ba9dfc25e588399
Saileeich <saile06@outlook.com>
2023-12-05 12:27:34 +0100

    Luke 5 i rust

---
+++ Luke_5_Summe-dele-primtall/target/debug/deps/Luke_5_Summe_dele_primtall-f231f698fca1a3f0.d
@@ -0,0 +1,5 @@
+C:\Users\saile\Desktop\KnowIt Kodekalender 2023\Luke_5_Summe-dele-primtall\target\debug\deps\libLuke_5_Summe_dele_primtall-f231f698fca1a3f0.rmeta: src\main.rs
+
+C:\Users\saile\Desktop\KnowIt Kodekalender 2023\Luke_5_Summe-dele-primtall\target\debug\deps\Luke_5_Summe_dele_primtall-f231f698fca1a3f0.d: src\main.rs
+
+src\main.rs:
